58e2bf96fd4b459aa7d52890eda9ed92aeed74cd
Aayush Kantak <120012051+aysh01@users.noreply.github.com>
2024-06-18 11:09:57 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -120,7 +120,7 @@ app.post('/login', async (req, res) => {
     }
 });
 
-const port = 5000 || process.env.PORT;
+const port = process.env.PORT || 5000;
 app.listen(port, () => {
     console.log(`Server running on Port ${port}`);
 })
